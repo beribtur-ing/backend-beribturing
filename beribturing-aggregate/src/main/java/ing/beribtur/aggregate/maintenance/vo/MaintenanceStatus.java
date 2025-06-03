e598518ef0da26298e369f21120ca9154032cc02
asrorbekobidjonov <asrorbek@nextree.io>
2025-06-03 09:06:23 +0500

    maintenance module is added to aggregate

---
+++ beribturing-aggregate/src/main/java/ing/beribtur/aggregate/maintenance/vo/MaintenanceStatus.java
@@ -0,0 +1,15 @@
+package ing.beribtur.aggregate.maintenance.vo;
+
+
+/**
+ * Represents the status of a maintenance request.
+ */
+public enum MaintenanceStatus {
+    REQUESTED,      // Maintenance has been requested but not yet reviewed
+    UNDER_REVIEW,   // Maintenance request is being reviewed
+    APPROVED,       // Maintenance request has been approved and will be performed
+    IN_PROGRESS,    // Maintenance is currently being performed
+    COMPLETED,      // Maintenance has been completed
+    REJECTED,       // Maintenance request has been rejected
+    CANCELLED       // Maintenance request has been cancelled by the requester
+}
