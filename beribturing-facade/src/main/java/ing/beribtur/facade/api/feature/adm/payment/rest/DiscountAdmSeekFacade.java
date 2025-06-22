29a125ae2358cf6003ba88ed64e36e449875d46d
Polat Sirojov <polat@nextree.io>
2025-06-22 13:04:45 +0500

    feat: update DiscountAdmSeekFacade and DiscountAdmSeekResource to use Discount entity instead of DiscountRdo for improved data handling

--- beribturing-facade/src/main/java/ing/beribtur/facade/api/feature/adm/payment/rest/DiscountAdmSeekFacade.java
+++ beribturing-facade/src/main/java/ing/beribtur/facade/api/feature/adm/payment/rest/DiscountAdmSeekFacade.java
@@ -1,15 +1,15 @@
 package ing.beribtur.facade.api.feature.adm.payment.rest;
 
 import ing.beribtur.accent.message.QueryResponse;
+import ing.beribtur.aggregate.payment.entity.Discount;
 import ing.beribtur.facade.api.feature.adm.payment.query.*;
-import ing.beribtur.facade.api.feature.adm.payment.sdo.DiscountRdo;
 
 import java.util.List;
 
 public interface DiscountAdmSeekFacade {
-    QueryResponse<DiscountRdo> findDiscount(FindDiscountAdmQuery query);
-    QueryResponse<List<DiscountRdo>> findDiscounts(FindDiscountsAdmQuery query);
-    QueryResponse<List<DiscountRdo>> findExpiredDiscounts(FindExpiredDiscountsAdmQuery query);
-    QueryResponse<List<DiscountRdo>> findValidDiscounts(FindValidDiscountsAdmQuery query);
-    QueryResponse<List<DiscountRdo>> findApplicableDiscounts(FindApplicableDiscountsAdmQuery query);
-} 
+    QueryResponse<Discount> findDiscount(FindDiscountAdmQuery query);
+    QueryResponse<List<Discount>> findDiscounts(FindDiscountsAdmQuery query);
+    QueryResponse<List<Discount>> findExpiredDiscounts(FindExpiredDiscountsAdmQuery query);
+    QueryResponse<List<Discount>> findValidDiscounts(FindValidDiscountsAdmQuery query);
+    QueryResponse<List<Discount>> findApplicableDiscounts(FindApplicableDiscountsAdmQuery query);
+}
--- beribturing-facade/src/main/java/ing/beribtur/facade/api/feature/adm/payment/rest/DiscountAdmSeekResource.java
+++ beribturing-facade/src/main/java/ing/beribtur/facade/api/feature/adm/payment/rest/DiscountAdmSeekResource.java
@@ -1,8 +1,8 @@
 package ing.beribtur.facade.api.feature.adm.payment.rest;
 
 import ing.beribtur.accent.message.QueryResponse;
+import ing.beribtur.aggregate.payment.entity.Discount;
 import ing.beribtur.facade.api.feature.adm.payment.query.*;
-import ing.beribtur.facade.api.feature.adm.payment.sdo.DiscountRdo;
 import ing.beribtur.feature.adm.payment.seek.DiscountAdmSeek;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/feature/admin/payment/discount")
@@ -21,76 +20,52 @@ public class DiscountAdmSeekResource implements DiscountAdmSeekFacade {
 
     @Override
     @PostMapping("/find/query")
-    public QueryResponse<DiscountRdo> findDiscount(@RequestBody FindDiscountAdmQuery query) {
+    public QueryResponse<Discount> findDiscount(@RequestBody FindDiscountAdmQuery query) {
         query.validate();
         var discount = discountAdmSeek.findDiscount(query.getDiscountId());
-        return new QueryResponse<>(DiscountRdo.fromDomain(discount));
+        return new QueryResponse<>(discount);
     }
 
     @Override
     @PostMapping("/find-multiple/query")
-    public QueryResponse<List<DiscountRdo>> findDiscounts(@RequestBody FindDiscountsAdmQuery query) {
+    public QueryResponse<List<Discount>> findDiscounts(@RequestBody FindDiscountsAdmQuery query) {
         query.validate();
-        List<DiscountRdo> discountRdos = List.of();
+        List<Discount> discounts = List.of();
 
         if (query.getDiscountIds() != null) {
-            var discounts = discountAdmSeek.findDiscounts(query.getDiscountIds());
-            discountRdos = discounts.stream()
-                    .map(DiscountRdo::fromDomain)
-                    .collect(Collectors.toList());
+            discounts = discountAdmSeek.findDiscounts(query.getDiscountIds());
         } else if (query.getOffset() != null) {
-            var discounts = discountAdmSeek.findDiscounts(query.getOffset());
-            discountRdos = discounts.stream()
-                    .map(DiscountRdo::fromDomain)
-                    .collect(Collectors.toList());
+            discounts = discountAdmSeek.findDiscounts(query.getOffset());
         } else if (query.getActive() != null) {
-            var discounts = discountAdmSeek.findActiveDiscounts();
-            discountRdos = discounts.stream()
-                    .map(DiscountRdo::fromDomain)
-                    .collect(Collectors.toList());
+            discounts = discountAdmSeek.findActiveDiscounts();
         } else if (query.getScope() != null) {
-            var discounts = discountAdmSeek.findByScope(query.getScope());
-            discountRdos = discounts.stream()
-                    .map(DiscountRdo::fromDomain)
-                    .collect(Collectors.toList());
+            discounts = discountAdmSeek.findByScope(query.getScope());
         } else if (query.getTargetId() != null) {
-            var discounts = discountAdmSeek.findByTargetId(query.getTargetId());
-            discountRdos = discounts.stream()
-                    .map(DiscountRdo::fromDomain)
-                    .collect(Collectors.toList());
+            discounts = discountAdmSeek.findByTargetId(query.getTargetId());
         }
 
-        return new QueryResponse<>(discountRdos);
+        return new QueryResponse<>(discounts);
     }
-    
+
     @Override
     @PostMapping("/find-expired/query")
-    public QueryResponse<List<DiscountRdo>> findExpiredDiscounts(FindExpiredDiscountsAdmQuery query) {
+    public QueryResponse<List<Discount>> findExpiredDiscounts(FindExpiredDiscountsAdmQuery query) {
         var discounts = discountAdmSeek.findExpiredDiscounts();
-        List<DiscountRdo> discountRdos = discounts.stream()
-            .map(DiscountRdo::fromDomain)
-            .collect(Collectors.toList());
-        return new QueryResponse<>(discountRdos);
+        return new QueryResponse<>(discounts);
     }
 
     @Override
     @PostMapping("/find-valid/query")
-    public QueryResponse<List<DiscountRdo>> findValidDiscounts(FindValidDiscountsAdmQuery query) {
+    public QueryResponse<List<Discount>> findValidDiscounts(FindValidDiscountsAdmQuery query) {
         var discounts = discountAdmSeek.findValidDiscounts();
-        List<DiscountRdo> discountRdos = discounts.stream()
-            .map(DiscountRdo::fromDomain)
-            .collect(Collectors.toList());
-        return new QueryResponse<>(discountRdos);
+        return new QueryResponse<>(discounts);
     }
 
     @Override
     @PostMapping("/find-applicable/query")
-    public QueryResponse<List<DiscountRdo>> findApplicableDiscounts(FindApplicableDiscountsAdmQuery query) {
+    public QueryResponse<List<Discount>> findApplicableDiscounts(FindApplicableDiscountsAdmQuery query) {
         query.validate();
         var discounts = discountAdmSeek.findApplicableDiscounts(query.getTargetId(), query.getScope());
-        List<DiscountRdo> discountRdos = discounts.stream()
-            .map(DiscountRdo::fromDomain)
-            .collect(Collectors.toList());
-        return new QueryResponse<>(discountRdos);
+        return new QueryResponse<>(discounts);
     }
 } 
