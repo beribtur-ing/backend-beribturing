f9d5a52d37ef349bd347a215e223dfd563048b79
Djuraev Mamurjon <mamurjon@nextree.io>
2025-07-01 20:27:15 +0500

    Add minio

--- beribturing-proxy/src/main/java/ing/beribtur/proxy/notisender/NotificationSenderService.java
+++ beribturing-proxy/src/main/java/ing/beribtur/proxy/notisender/NotificationSenderService.java
@@ -1,7 +1,35 @@
 package ing.beribtur.proxy.notisender;
 
+import ing.beribtur.aggregate.notification.entity.Notification;
+import ing.beribtur.config.nats.NatsSubjectManagerService;
+import ing.beribtur.proxy.nats.NatsPublisherService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationSenderService {
+    //
+    @Value("${nats.subjects.notification}")
+    private final String subjectsNotification;
+    private final NatsPublisherService natsPublisherService;
+    private final NatsSubjectManagerService natsSubjectManagerService;
+
+    public NotificationSenderService(String subjectsNotification, NatsPublisherService natsPublisherService, NatsSubjectManagerService natsSubjectManagerService) {
+        //
+        this.subjectsNotification = subjectsNotification;
+        this.natsPublisherService = natsPublisherService;
+        this.natsSubjectManagerService = natsSubjectManagerService;
+    }
+
+    public void registerUserForNotification(String user) {
+        //
+        String streamName = String.format("%s-%s", user, "notification");
+        this.natsSubjectManagerService.addUserSubjectToStream(streamName, user);
+    }
+
+    public void sendNotificationToUser(String user, Notification notification) {
+        //
+        String subject = String.format("%s.%s", subjectsNotification, user);
+        this.natsPublisherService.publish(subject, notification.toJson());
+    }
 }
